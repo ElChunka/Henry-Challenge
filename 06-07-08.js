e29150ddccd8dcbebe8cc7b0bb291e68d03c84aa
ElChunka <lucas05091987@gmail.com>
2022-06-18 20:49:45 -0300

    puntos 6,7 y 8

--- 06-07-08.js
+++ 06-07-08.js
@@ -11,6 +11,10 @@ function crearClasePersona() {
       // Inicializar las propiedades de la persona con los valores recibidos como argumento
 
       // Tu código aca:
+      this.nombre = nombre;
+      this.edad = edad;
+      this.hobbies = hobbies;
+      this.amigos = amigos;
 
     }
 
@@ -20,6 +24,8 @@ function crearClasePersona() {
       // No debe retornar nada.
 
       // Tu código aca:
+      var amigo = {nombre, edad};
+      this.amigos.push(amigo);
 
     }
 
@@ -28,6 +34,7 @@ function crearClasePersona() {
       // No debe retornar nada.
 
       // Tu código aca:
+      this.hobbies.push(hobby);
 
     }
     getFriends() {
@@ -38,6 +45,11 @@ function crearClasePersona() {
       // persona.getFriends() debería devolver ['martin', 'toni']
 
       // Tu código aca:
+      
+      let nombresAmigos = this.amigos.map(amigo => amigo.nombre);
+      return nombresAmigos;
+
+    
 
     }
 
@@ -47,7 +59,9 @@ function crearClasePersona() {
       // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
 
       // Tu código aca:
-
+      //let hobbiesPersona = this.amigos.map(amigo => amigo.hobbies);
+      return this.hobbies;
+     
     }
 
     getPromedioEdad() {
@@ -66,6 +80,10 @@ function crearClasePersona() {
       // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
 
       // Tu código aca:
+      var sumaEdad = 0;
+      this.amigos.forEach(element => sumaEdad += element.edad);
+      var prom = sumaEdad/this.amigos.length;
+      return prom;
 
     }
   };
